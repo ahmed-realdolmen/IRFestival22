422e56a98eab56d5a85e9ff58af8ba5f568df3de
Ahmed Abou Zaidi <Ahmed.AbouZaidi@inetum-realdolmen.world>
2022-10-17 13:31:35 +0200

    Update apiBaseUrl

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: "IRFestival",
-  apiBaseUrl: "https://irfestivalapiahmed.azurewebsites.net/api/"
+  apiBaseUrl: "https://irfestivalapimahmed.azure-api.net/2022/api/"
 };
